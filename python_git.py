71ad242bc2b197d74908001e0e6020643c561828
balasharan <sharankumar526@gmail.com>
2019-12-05 10:21:52 +0530

    2commit

--- python_git.py
+++ python_git.py
@@ -1,6 +1,10 @@
 #from git import Repo
 import subprocess
 a = 'qa'
+text = 'qa12-012'
+with open("test2", "a") as myfile:
+    myfile.write(text)
+
 if ( a == 'master' ):
     subprocess.check_output('git checkout master', shell=True)
     subprocess.check_output('git add .', shell=True)
@@ -9,5 +13,5 @@ if ( a == 'master' ):
 else:
     subprocess.check_output('git checkout qa', shell=True)
     subprocess.check_output('git add .', shell=True)
-    subprocess.check_output('git commit -m "1 commit"', shell=True)
+    subprocess.check_output('git commit -m "2commit"', shell=True)
     subprocess.check_output('git push origin qa', shell=True)
