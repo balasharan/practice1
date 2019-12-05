1ee0b4c400112fadcb615d8c319d9bae053f6efd
balasharan <sharankumar526@gmail.com>
2019-12-05 10:08:30 +0530

    1 commit

--- python_git.py
+++ python_git.py
@@ -1,5 +1,13 @@
 #from git import Repo
 import subprocess
-subprocess.check_output('git add .', shell=True)
-subprocess.check_output('git commit -m "a commit"', shell=True)
-subprocess.check_output('git push origin master', shell=True)
+a = 'qa'
+if ( a == 'master' ):
+    subprocess.check_output('git checkout master', shell=True)
+    subprocess.check_output('git add .', shell=True)
+    subprocess.check_output('git commit -m "1 commit"', shell=True)
+    subprocess.check_output('git push origin master', shell=True)
+else:
+    subprocess.check_output('git checkout qa', shell=True)
+    subprocess.check_output('git add .', shell=True)
+    subprocess.check_output('git commit -m "1 commit"', shell=True)
+    subprocess.check_output('git push origin qa', shell=True)
