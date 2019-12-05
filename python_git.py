360170681e171846b941c867c11a323786c20c3a
balasharan <sharankumar526@gmail.com>
2019-12-05 11:08:34 +0530

    7 th commit

--- python_git.py
+++ python_git.py
@@ -2,15 +2,19 @@
 import subprocess
 a = 'qa'
 text = 'qa12-16 \n'
-with open("test2", "a") as myfile:
-    myfile.write(text)
+# with open("test2", "a") as myfile:
+#     myfile.write(text)
 
 if ( a == 'master' ):
+    with open("test", "a") as myfile:
+        myfile.write(text)
     subprocess.check_output('git checkout master', shell=True)
     subprocess.check_output('git add .', shell=True)
-    subprocess.check_output('git commit -m "1 commit"', shell=True)
+    subprocess.check_output('git commit -m "2 commit"', shell=True)
     subprocess.check_output('git push origin master', shell=True)
 else:
+    with open("test2", "a") as myfile:
+        myfile.write(text)
     subprocess.check_output('git checkout qa', shell=True)
     subprocess.check_output('git add .', shell=True)
     subprocess.check_output('git commit -m "7 th commit"', shell=True)
