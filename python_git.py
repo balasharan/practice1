8571df4dfba55abbc293ea5fad00ef0a4351b46e
balasharan <sharankumar526@gmail.com>
2019-12-05 09:41:31 +0530

    c commit

--- python_git.py
+++ python_git.py
@@ -1,5 +1,5 @@
 #from git import Repo
 import subprocess
 subprocess.check_output('git add .', shell=True)
-subprocess.check_output('git commit -m "b commit"', shell=True)
+subprocess.check_output('git commit -m "c commit"', shell=True)
 subprocess.check_output('git push origin master', shell=True)
