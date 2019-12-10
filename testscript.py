1ad1d8e13225bd9ed1b56a40cced420ebc9de6f9
balasharan <sharankumar526@gmail.com>
2019-12-10 12:00:05 +0530

    111 commit

--- testscript.py
+++ testscript.py
@@ -4,9 +4,10 @@ def subprocess_cmd(command):
     proc_stdout = process.communicate()[0].strip()
     print(proc_stdout)
 
-a = 'dev'
-text = 'qa12-24445wertyuytr \n'
+branch_name = 'dev'
+text = 'qa12-24445wertyuytrjhgfde \n'
 with open("test_1", "w") as myfile:
     myfile.write(text)
 
-subprocess_cmd('git checkout -b dev; git add .; git commit -m "111 commit";git push origin dev')
+
+subprocess_cmd('git checkout -b $branch_name; git add .; git commit -m "111 commit";git push origin $branch_name')
