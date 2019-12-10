ada70518d26f0472789affa263407f76852d2481
balasharan <sharankumar526@gmail.com>
2019-12-10 11:31:46 +0530

    check

--- python_git.py
+++ python_git.py
@@ -1,5 +1,18 @@
-#from git import Repo
 import subprocess
-subprocess.check_output('git add .', shell=True)
-subprocess.check_output('git commit -m "c commit"', shell=True)
-subprocess.check_output('git push origin master', shell=True)
+a = 'master'
+text = 'qa12-22 \n'
+with open("test", "a") as myfile:
+    myfile.write(text)
+try:
+    if ( a == 'master' ):
+        subprocess.check_output('git checkout -f master', shell=True)
+        subprocess.check_output('git add .', shell=True)
+        subprocess.check_output('git commit -m "111 commit"', shell=True)
+        subprocess.check_output('git push origin master', shell=True)
+    else:
+        subprocess.check_output('git checkout qa', shell=True)
+        subprocess.check_output('git add .', shell=True)
+        subprocess.check_output('git commit -m "23132 th commit"', shell=True)
+        subprocess.check_output('git push origin qa', shell=True)
+except subprocess.CalledProcessError as error:
+    print(error)
