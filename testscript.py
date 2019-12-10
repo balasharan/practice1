64ff24bda97eb3aa3799a90b2426de6f36605f16
balasharan <sharankumar526@gmail.com>
2019-12-10 11:52:42 +0530

    111 commit

--- testscript.py
+++ testscript.py
@@ -4,9 +4,9 @@ def subprocess_cmd(command):
     proc_stdout = process.communicate()[0].strip()
     print(proc_stdout)
 
-a = 'master'
+a = 'dev'
 text = 'qa12-22 \n'
 with open("test", "a") as myfile:
     myfile.write(text)
 
-subprocess_cmd('git checkout -f master; git add .; git commit -m "111 commit";git push origin master')
+subprocess_cmd('git checkout -b dev; git add .; git commit -m "111 commit";git push origin master')
